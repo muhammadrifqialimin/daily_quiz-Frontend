61efe66fb3ce490040edb951e0b33fda3a5f6f2d
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-02-10 03:51:02 +0700

    menambahkan layanan StudentService untuk menangani proses login pengguna

---
+++ lib/services/student_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../core/api_config.dart'; // Import config tadi
+
+class StudentService {
+  Future<Map<String, dynamic>> login(String name, String password) async {
+    try {
+      final response = await http.post(
+        Uri.parse('${ApiConfig.baseUrl}/login'),
+        body: jsonEncode({'name': name, 'password': password}),
+        headers: ApiConfig.headers,
+      );
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Login Gagal: Nama atau Password salah');
+      }
+    } catch (e) {
+      throw Exception('Error Login: $e');
+    }
+  }
+}
